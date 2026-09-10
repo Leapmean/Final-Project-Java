9cef87d8bf7a3c0276145b418c13b0ba507d8c14
Kaknika9 <kaknika189999@icloud.com>
2026-09-10 14:11:28 +0700

    feat: LibraryFrame.java change output: JMenu bar: JMenuItem

--- src/main/java/dev/leapmean/LibraryFrame.java
+++ src/main/java/dev/leapmean/LibraryFrame.java
@@ -22,12 +22,12 @@ public class LibraryFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JMenuBar bar = new JMenuBar();
-        JMenu file = new JMenu("File");
-        JMenuItem refresh = new JMenuItem("Display");
-        JMenuItem add = new JMenuItem("New");
-        JMenuItem update = new JMenuItem("Update");
-        JMenuItem delete = new JMenuItem("Delete");
-        JMenuItem exit = new JMenuItem("Exit");
+        JMenu file = new JMenu("Library");
+        JMenuItem refresh = new JMenuItem("View Books");
+        JMenuItem add = new JMenuItem("Add Book");
+        JMenuItem update = new JMenuItem("Edit Book");
+        JMenuItem delete = new JMenuItem("Remove Book");
+        JMenuItem exit = new JMenuItem("Exit: Good Luck!");
 
         refresh.addActionListener(e -> loadBooks());
         add.addActionListener(e -> addBook());
